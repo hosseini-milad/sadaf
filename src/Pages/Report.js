c98c4fe317126caf22880c1ba15a315e946cfbff
milad <milad.cia@gmail.com>
2024-07-15 12:10:47 +0330

    fPage

--- src/Pages/Report.js
+++ src/Pages/Report.js
@@ -1,13 +1,14 @@
+import { useState } from "react"
 import ReportFilter from "../modules/Report/ReportFilter"
 import ReportHolder from "../modules/Report/ReportHolder"
 
 function Report(props){
-    
+   const [filter,setFilter] = useState()
    return(
       <main className="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_module">
          <div className="ReportMainHolder">
-            <div className="ReportFilter"><ReportFilter /></div>
-            <div className="ReportHolder"><ReportHolder />  </div> 
+            <div className="ReportFilter"><ReportFilter setFilter={setFilter}/></div>
+            <div className="ReportHolder"><ReportHolder filter={filter}/>  </div> 
          </div>   
       </main>
    )
--- src/modules/FindIdea/SingleIdea.jsx
+++ src/modules/FindIdea/SingleIdea.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
+
 function SingleIdea(props){
     const data = props.data
+    const [showMore,setMore] = useState(0)
     return(
         <div className="wf-product-cards__card cl-card -light -container-01 -hoverable ">
               <div className="wf-product-cards__content">
                 <div className="wf-product-cards__heading--wrapper">
                   <img src="/img/hub/MarketingHub_Icon_2023_Gradient_RGB_24px.svg" alt="" width="30" height="30" loading="lazy" className="wf-product-cards__heading--image"/>
-                  <h3 className="wf-product-cards__heading twolineText">
+                  <h3 className="wf-product-cards__heading twolineText" title={data.title}>
                     {data.title} 
                   </h3>
                 </div>
@@ -13,8 +16,22 @@ function SingleIdea(props){
                 <div className="wf-product-cards__features">
                   <h4 className="wf-product-cards__feature-list--heading ">
                     <i className="fa fa-user ideaIcon" ></i>
-                    {data.malek}
+                    <small>{data.malek?data.malek[0]:''}
+                    </small>
+                    <sub onClick={()=>setMore(1)}> 
+                      ({data.malek.length} مخترع) </sub>
+                    {showMore?<div className="malekPopHolder">
+                      <div className="malekPop">
+                        <i className="fa fa-close malekClose"
+                       onClick={()=>setMore(0)}></i>
+                      {data.malek&&data.malek.map((malek,i)=>(<>
+                        <i className="fa fa-user ideaIcon" ></i>
+                        <small key={i}>{malek}</small><br/></>
+                      ))}
+                    </div>
+                  </div>:<></>}
                   </h4>
+                 
                   <div className="wf-product-cards__feature-list--items">
                     <ul>
                       <li className="wf-product-cards__feature-list--item">
