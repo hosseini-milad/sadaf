ecd8f2f1c4ae408573a5eb4bef6b523ca6badf4e
milad <milad.cia@gmail.com>
2024-09-27 14:01:48 +0330

    transactions

--- middleware/CheckActive.js
+++ middleware/CheckActive.js
@@ -10,7 +10,7 @@ function CheckActive(coWorkData) {
 
     var defDay = parseInt(defDate/(24*60*60*1000))
     if(defDay<30)
-      return(defDay)
+      return(30-defDay)
     return(0)
 }
 module.exports =CheckActive
