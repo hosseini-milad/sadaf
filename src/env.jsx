9c3cd1217c1fb1bf1feff78cb9b8f127a13803d8
milad.cia <milad.cia@gmail.com>
2024-11-04 00:21:24 +0330

    cahnges

--- src/env.jsx
+++ src/env.jsx
@@ -1,10 +1,12 @@
 
 const env={
-    siteApi:'http://localhost:2020/api',
+    //siteApi:'http://localhost:2020/api',
     //siteApi:'https://reserveadmin.qomstp.ir/api',
+    siteApi:'http://2.179.166.169:8088/api',
     
-    siteApiUrl:'http://localhost:2020',
+    //siteApiUrl:'http://localhost:2020',
     //siteApiUrl:'https://reserveadmin.qomstp.ir',
+    siteApiUrl:'http://2.179.166.169:8088',
     cookieName:'sadaf-admin-login',
     //cookieName:'panel-login',
     //cookieName:'mehr-login',
