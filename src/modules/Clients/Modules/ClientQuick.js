996f625e77fd86759a24ba09862ca1ffaae895dc
hosseini.milad1400 <milad.cia@gmail.com>
2025-07-05 23:51:25 +0330

    cart id

--- src/modules/Clients/Modules/ClientQuick.js
+++ src/modules/Clients/Modules/ClientQuick.js
@@ -1,10 +1,15 @@
+import env from "../../../env"
+
 function ClientQuick(props){
     const data = props.data
+    console.log(data)
     return(
       <div className="sub-order-table">
             <div className="sub-avatar inLine">
-                <div className="sub-avatar-container">
-                    
+                <div className="imageShow">
+                    <span>کدملی: {data.meliCode}</span>
+                    {data.meliImage?
+                    <img src={env.siteApiUrl+data.meliImage}/>:<></>}
                 </div>
                 <div className="sub-num">
                     <p>وضعیت رزرو: {data.regCode}</p>
