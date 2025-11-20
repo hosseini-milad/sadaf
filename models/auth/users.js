39ea7dee79d0f5014624fc33b34cf4f93e38083d
hosseini.milad1400 <milad.cia@gmail.com>
2025-11-20 11:38:32 +0330

    access control

--- models/auth/users.js
+++ models/auth/users.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
   access:{
     type:String
   },
+  profile:{ type: String },
   group: { type:String },
   credit: { type: String },
   token: { type: String },
